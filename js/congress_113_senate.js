4a2c8c0f828eabe7e5fb562d100a21d03e60b31c
LianLopez <lopez.leonardo.info@gmail.com>
2019-05-22 08:57:41 -0300

    data response json

--- js/congress_113_senate.js
+++ js/congress_113_senate.js
@@ -1,24 +1,24 @@
-var url = "https://api.propublica.org/congress/v1/members/";
-var api = "SKzNT67W2EOm73kAt8h7eqwaWVvRM8T1SsCQKY7v";
-
+var url = "https://api.propublica.org/congress/v1/";
 
 var data = $(function() {
-            var misCabeceras = new Headers({
-            	'api':'SKzNT67W2EOm73kAt8h7eqwaWVvRM8T1SsCQKY7v'
-            });
+    var misCabeceras = new Headers({
+        'X-API-Key': 'SKzNT67W2EOm73kAt8h7eqwaWVvRM8T1SsCQKY7v'
+    });
 
-            var miInit = {
-                method: 'GET',
-                headers: misCabeceras,
-                mode: 'cors',
-                cache: 'default'
-            };
+    var miInit = {
+        method: 'GET',
+        headers: misCabeceras,
+        mode: 'cors',
+        cache: 'default'
+    };
 
-            fetch(url, miInit)
-                .then(function(response) {
-                    return response.json();
-                })
-                .then(function(myJson) {
-                    console.log(myJson);
-                });
-              });
+    fetch(url, miInit)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            console.log(myJson);
+        });
+        .catch(function() { console.log("error"); 
+      	});
+});
